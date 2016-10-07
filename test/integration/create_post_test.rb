bf868005ccae6aa1e55cd6110987ea56e1601c0c
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-10-07 10:13:42 +0100

    fix test

--- test/integration/create_post_test.rb
+++ test/integration/create_post_test.rb
@@ -6,22 +6,22 @@ class CreatePostTest < ActiveSupport::TestCase
   end
 
   def test_create_new_post
-    fill_in("Title", :with => "True title")
-    fill_in("Body", :with => "TEXT TEXT TEXT"*20)
+    fill_in("post_title", :with => "True title")
+    fill_in("post_body", :with => "TEXT TEXT TEXT"*20)
     click_button "Create Post"
     assert_equal 1, Post.all.count
   end
 
   def test_show_error_message_when_post_should_not_be_created_because_of_the_title_wrong
-    fill_in("Title", :with => "F")
-    fill_in("Body", :with => "TEXT TEXT TEXT"*20)
+    fill_in("post_title", :with => "F")
+    fill_in("post_body", :with => "TEXT TEXT TEXT"*20)
     click_button "Create Post"
     assert page.has_content?("Sorry, your post is not valid. Please, try again.")
   end
 
   def test_show_error_message_when_post_should_not_be_created_because_of_the_body_wrong
-    fill_in("Title", :with => "True title")
-    fill_in("Body", :with => "T"*20)
+    fill_in("post_title", :with => "True title")
+    fill_in("post_body", :with => "T"*20)
     click_button "Create Post"
     assert page.has_content?("Sorry, your post is not valid. Please, try again.")
   end
--- test/integration/edit_post_test.rb
+++ test/integration/edit_post_test.rb
@@ -8,25 +8,25 @@ class EditPostTest < ActiveSupport::TestCase
   end
 
   def test_good_edit_post_title_and_see_upgrade
-    fill_in("Title", :with => "True title1")
+    fill_in("post_title", :with => "True title1")
     click_button "Update Post"
     assert page.has_content?("True title1")
   end
 
   def test_good_edit_post_body_and_see_upgrade
-    fill_in("Body", :with => "TEXT1 TEXT1 TEXT1"*20)
+    fill_in("post_body", :with => "TEXT1 TEXT1 TEXT1"*20)
     click_button "Update Post"
     assert page.has_content?("TEXT1 TEXT1 TEXT1"*20)
   end
 
   def test_bad_edit_post_title_and_see_error
-    fill_in("Title", :with => "F")
+    fill_in("post_title", :with => "F")
     click_button "Update Post"
     assert page.has_content?("Sorry, your update is not valid. Please, try again.")
   end
 
   def test_bad_edit_post_body_and_see_error
-    fill_in("Body", :with => "F")
+    fill_in("post_body", :with => "F")
     click_button "Update Post"
     assert page.has_content?("Sorry, your update is not valid. Please, try again.")
   end
