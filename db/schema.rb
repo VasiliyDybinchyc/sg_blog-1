78bf81332016325a7fbe41fcafc9b12e38091351
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-10-08 08:30:29 +0100

    Create and run migration to add image to posts and generate uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919052148) do
+ActiveRecord::Schema.define(version: 20161008072409) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.string "image"
   end
 
 end
