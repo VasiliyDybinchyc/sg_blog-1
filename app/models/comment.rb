6778b3051831d6f5b62cd1b50b930cdfc60bb9d5
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-10-20 12:45:14 +0100

    fix create comment and add validaton comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  validates :body, length: { in: 5..200 }
+  PROTECTION_FORM_LINKS_REGEX = /\A(https|http|ftp|ftps)*:*+[\/\/]*+[\w+\-]+\.[a-z]+[\/\w+\/]*+\z/i
+  PROTECTION_FORM_FIRST_POST_REGEX = /\A[^`]*(First post+[^`]*)\z/i
+  validates :body, length: { minimum: 2 }, format: { without: PROTECTION_FORM_FIRST_POST_REGEX,
+                                                     without: PROTECTION_FORM_LINKS_REGEX}
 end
