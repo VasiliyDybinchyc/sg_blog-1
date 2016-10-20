0c55775d5580b5250861bf3d888559ae4b142e59
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-10-20 14:01:54 +0100

    finish Mini Home Work

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   PROTECTION_FORM_LINKS_REGEX = /\A(https|http|ftp|ftps)*:*+[\/\/]*+[\w+\-]+\.[a-z]+[\/\w+\/]*+\z/i
   PROTECTION_FORM_FIRST_POST_REGEX = /\A[^`]*(First post+[^`]*)\z/i
-  validates :body, length: { minimum: 2 }, format: { without: PROTECTION_FORM_FIRST_POST_REGEX,
-                                                     without: PROTECTION_FORM_LINKS_REGEX}
+  validates :body, length: { minimum: 2 }, format: { without: PROTECTION_FORM_LINKS_REGEX }
+  validates :body, format: { without: PROTECTION_FORM_FIRST_POST_REGEX }
 end
