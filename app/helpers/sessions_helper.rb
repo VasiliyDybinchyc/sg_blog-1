a7cccf9fbe877fd7630fbe82fbf8ef42b34cd3a5
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-11-05 21:23:56 +0000

    again fix views and user can't change location another user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,14 @@ module SessionsHelper
     end
   end
 
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
   def logged_in?
     !current_user.nil?
   end
